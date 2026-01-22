d9565f82bd5ffd824ae5cf830dbe988a8f13b17c
xuyisen <xuyisen123456@outlook.com>
2026-01-22 20:25:13 +0800

    feat: Implement GlossaryList component with RuleToggleButton for DnD terminology

--- frontend/src/components/MainLayout/CharacterSheetDnD5/GlossaryList.vue
+++ frontend/src/components/MainLayout/CharacterSheetDnD5/GlossaryList.vue
@@ -1,1 +1,43 @@
-<template><div>这是法术速查界面</div></template>
+<script setup lang="ts">
+import RuleToggleButton from './Common/RuleToggleButton.vue'
+</script>
+
+<template>
+  <div class="glossary-list">
+    <header class="header">
+      <div class="title">
+        <h1>术语速查表</h1>
+        <RuleToggleButton />
+      </div>
+    </header>
+
+    <div>TODO</div>
+  </div>
+</template>
+
+<style scoped>
+.spell-list {
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  justify-content: space-between;
+}
+
+.title h1 {
+  margin: 0;
+  font-size: 32px;
+  color: var(--dnd-dragon-red);
+}
+</style>
